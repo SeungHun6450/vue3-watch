bf273a4c8c45279abf0ac409169013e02b4c8c28
SeungHun6450 <toffg6450@naver.com>
2021-06-14 19:17:49 +0900

    watch

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1 @click="changeMessage">
+    {{ msg }}
+  </h1>
+  <h1>{{ reversedMessage }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello?'
+    }
+  },
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  watch: {
+    // 해당하는 부분을 감시하고 있어서 바뀐 내용이 감지가 되면 바뀐것을 볼 수 있다.
+    msg(value) {
+      // console.log('msg : ', this.msg)
+      console.log('msg : ', value)
+    },
+    reversedMessage(value) {
+      // console.log('reversedMessage : ', this.reversedMessage)
+      console.log('reversedMessage : ', value)
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.msg = 'Good!'
+    }
+  }
+}
+</script>
